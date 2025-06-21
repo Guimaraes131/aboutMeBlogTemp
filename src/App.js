f6d3662cf26dac2d1fb9d1efe14c5b3f8c1d08b4
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-21 07:18:34 -0300

    Add biblioteca react-router-dom e mapeando rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./paginas/Home";
+import SobreMim from "./paginas/SobreMim";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<SobreMim />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/paginas/SobreMim/index.js
@@ -0,0 +1,8 @@
+
+const SobreMim = () => {
+  return (
+    <h1>SobreMim</h1>
+  )
+}
+
+export default SobreMim
